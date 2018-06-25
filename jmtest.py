a2cde3b20d4da72e6c7791670bbd35a7cc0544c5
markbejosano <mbejosano@west.com>
2018-06-25 14:04:12 -0500

    Update jmtest.py

--- jmtest.py
+++ jmtest.py
@@ -1,7 +1,7 @@
 import os
 import os.path
 
-check_file = os.path.isfile("HTTPRequest.jmx"):
+check_file = os.path.isfile("HTTPRequest.jmx")
 if check_file=="True":
   y = os.path.abspath("./HTTPRequest.jmx")
   z = y.replace('\\','\\\\') 
